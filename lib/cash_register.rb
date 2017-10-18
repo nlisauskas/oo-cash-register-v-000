c2180297e3eda26d3797edb3f9ecc87b7617fa4c
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-18 00:46:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :last_item
+
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += (price * quantity)
+    @last_item = price
+  end
+
+end
